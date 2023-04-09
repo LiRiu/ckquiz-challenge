5ce88df00295d8f929bd215382a23d4ecfd1ec66
LiRiu <congfei.li@chaitin.com>
2023-04-10 00:00:53 +0800

    feat(app): auto switch network

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { MetaMaskWallet } from "@thirdweb-dev/wallets";
 import './index.css';
 import {
   Group,
   TextInput,
   IconButton,
-  SearchIcon,
   Spinner,
   LogInIcon,
   ArchiveIcon,
@@ -22,31 +22,83 @@ export function App() {
   const [isInvalidAns, setIsInvalidAns] = useState(false);
   const [quizAns, setQuizAns] = useState("");
   const [txHash, setTxHash] = useState("");
+
+  const Godwoken = {
+    // === Required information for connecting to the network === \\
+    chainId: 71401, // Chain ID of the network
+    // Array of RPC URLs to use
+    rpc: ["https://godwoken-testnet-v1.ckbapp.dev"],
+
+    // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+    // Information about the chains native currency (i.e. the currency that is used to pay for gas)
+    nativeCurrency: {
+      decimals: 18,
+      name: "Godwoken CKB",
+      symbol: "CKB",
+    },
+    shortName: "Godwoken", // Display value shown in the wallet UI
+    slug: "Godwoken", // Display value shown in the wallet UI
+    testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+    chain: "Godwoken", // Name of the network
+    name: "Godwoken", // Name of the network
+  }
+
+  const wallet = new MetaMaskWallet({
+    chains: [ Godwoken ],
+    dappMetadata: {
+      name: "CKQuiz",
+      url: "https://ckquiz-home.vercel.app/",
+      description: "Challenge for CKB",
+      logoUrl: "https://ckquiz-home.vercel.app/favicon.ico",
+    },
+  });
+
   const url = window.location.search.split("?")[1];
   const quizId = url.split("=")[1].split("&")[0];
   const quizText = url.split("=")[2];
 
   useEffect(() => {
     asyncSleep(100).then(() => {
-      if (ethereum.selectedAddress) connectToMetaMask();
+      if (ethereum.selectedAddress){ 
+        connectToMetaMask();
+      }
       ethereum.addListener("accountsChanged", connectToMetaMask);
     });
   }, []);
+
+  function connectToMetaMask() {
+    wallet.connect().then(()=>{
+      wallet.switchChain(71401).then(()=>{
+        setEthAddr(ethereum.selectedAddress);
+      }).catch(()=>{
+        addGodwokenToMetaMask();
+      });
+    })
+  }
+
+  function addGodwokenToMetaMask() {
+    window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [{
+        chainId: "0x116e9",
+        rpcUrls: ["https://godwoken-testnet-v1.ckbapp.dev"],
+        chainName: "Godwoken",
+        nativeCurrency: {
+          name: "CKB",
+          symbol: "CKB",
+          decimals: 18
+        },
+        blockExplorerUrls: ["https://v1.testnet.gwscan.com/"]
+      }]
+    });
+  }
 
   function asyncSleep(ms){
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  function connectToMetaMask() {
-    ethereum
-      .enable()
-      .then(([ethAddr]) => {
-        setEthAddr(ethAddr);
-      })
-  }
-
   function handleClick(){
-    if(ethereum.selectedAddress && !isDone && !isSending){
+    if(ethAddr && !isDone && !isSending){
       setIsSending(true);
 
       function succ(e){
